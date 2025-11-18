0c1a267b6fa061fdeade7e13fd004ba453d2699f
Anderson <andersonlimacrv@gmail.com>
2025-11-17 23:32:12 -0300

    feat @ add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,34 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/layout/header";
+
 
 function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+ return (
+    <div className="w-full">
+      <Header />
+
+      <main className="mx-auto min-h-screen w-full max-w-3xl px-4 py-12">
+        <div className="space-y-2 mb-4">
+          <div className="bg-accent h-6 w-4/6 rounded-md border" />
+          <div className="bg-accent h-6 w-1/2 rounded-md border" />
+        </div>
+        <div className="flex gap-2 mb-8">
+          <div className="bg-accent h-3 w-14 rounded-md border" />
+          <div className="bg-accent h-3 w-12 rounded-md border" />
+        </div>
+
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div key={i} className="space-y-2 mb-8">
+            <div className="bg-accent h-4 w-full rounded-md border" />
+            <div className="bg-accent h-4 w-full rounded-md border" />
+            <div className="bg-accent h-4 w-full rounded-md border" />
+            <div className="bg-accent h-4 w-1/2 rounded-md border" />
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ frontend/src/components/layout/header.tsx
@@ -0,0 +1,179 @@
+'use client';
+import React from 'react';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { MenuToggleIcon } from '@/components/ui/menu-toggle-icon';
+import { useScroll } from '@/hooks/use-scroll';
+
+export function Header() {
+	const [open, setOpen] = React.useState(false);
+	const scrolled = useScroll(10);
+
+	const links = [
+		{
+			label: 'Features',
+			href: '#',
+		},
+		{
+			label: 'Pricing',
+			href: '#',
+		},
+		{
+			label: 'About',
+			href: '#',
+		},
+	];
+
+	React.useEffect(() => {
+		if (open) {
+			// Disable scroll
+			document.body.style.overflow = 'hidden';
+		} else {
+			// Re-enable scroll
+			document.body.style.overflow = '';
+		}
+
+		// Cleanup when component unmounts (important for Next.js)
+		return () => {
+			document.body.style.overflow = '';
+		};
+	}, [open]);
+
+	return (
+    <header
+      className={cn(
+        "sticky top-0 z-50 mx-auto w-full max-w-5xl border-b border-transparent md:rounded-md md:border md:transition-all md:ease-out",
+        {
+          "bg-background/95 supports-backdrop-filter:bg-background/50 border-border backdrop-blur-lg md:top-4 md:max-w-4xl md:shadow":
+            scrolled && !open,
+          "bg-background/90": open,
+        }
+      )}
+    >
+      <nav
+        className={cn(
+          "flex h-14 w-full items-center justify-between px-4 md:h-12 md:transition-all md:ease-out",
+          {
+            "md:px-2": scrolled,
+          }
+        )}
+      >
+        <div className="flex items-center">
+          <MateUpLogo className="h-8" />
+          <p className="-rotate-45">🚀</p>
+        </div>
+        <div className="hidden items-center gap-2 md:flex">
+          {links.map((link, i) => (
+            <a
+              key={i}
+              className={buttonVariants({ variant: "ghost" })}
+              href={link.href}
+            >
+              {link.label}
+            </a>
+          ))}
+          <Button variant="outline">Sign In</Button>
+          <Button>Get Started</Button>
+        </div>
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={() => setOpen(!open)}
+          className="md:hidden"
+        >
+          <MenuToggleIcon open={open} className="size-5" duration={300} />
+        </Button>
+      </nav>
+
+      <div
+        className={cn(
+          "bg-background/90 fixed top-14 right-0 bottom-0 left-0 z-50 flex flex-col overflow-hidden border-y md:hidden",
+          open ? "block" : "hidden"
+        )}
+      >
+        <div
+          data-slot={open ? "open" : "closed"}
+          className={cn(
+            "data-[slot=open]:animate-in data-[slot=open]:zoom-in-95 data-[slot=closed]:animate-out data-[slot=closed]:zoom-out-95 ease-out",
+            "flex h-full w-full flex-col justify-between gap-y-2 p-4"
+          )}
+        >
+          <div className="grid gap-y-2">
+            {links.map((link) => (
+              <a
+                key={link.label}
+                className={buttonVariants({
+                  variant: "ghost",
+                  className: "justify-start",
+                })}
+                href={link.href}
+              >
+                {link.label}
+              </a>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Button variant="outline" className="w-full">
+              Sign In
+            </Button>
+            <Button className="w-full">Get Started</Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export const MateUpLogo = (props: React.ComponentProps<"svg">) => (
+  <svg
+    viewBox="0 0 420 160"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g
+      textAnchor="middle"
+      fontFamily="Inter, sans-serif"
+      dominantBaseline="middle"
+    >
+      {/* MATE */}
+      <text
+        x="30%"
+        y="45%"
+        fontWeight="900"
+        fontSize="72"
+        opacity="0.8"
+        letterSpacing="6px"
+      >
+        mate
+      </text>
+
+      {/* UP */}
+      <text
+        x="75%"
+        y="45%"
+        className="text-[#0ACACA] animate-pulse duration-1000 ease-in-out"
+        fontWeight="900"
+        fontSize="100"
+        letterSpacing="6px"
+      >
+        Up
+      </text>
+    </g>
+    {/* LEAGUE */}{" "}
+    <text
+      x="60%"
+      y="90%"
+      textAnchor="middle"
+      fontFamily="Inter, sans-serif"
+      fontWeight="800"
+      fontSize="50"
+      opacity="0.8"
+      letterSpacing="8px"
+      dominantBaseline="middle"
+      fill="#ae0538"
+    >
+      LEAGUE
+    </text>
+  </svg>
+);
